b3b01577f5e6f705ac5dc21c8687d7b9a0a990d2
p0stro <110983036+p0stro@users.noreply.github.com>
2022-08-23 12:43:38 +0200

    Add forecast columns dynamically

--- script.js
+++ script.js
@@ -27,6 +27,38 @@ function displayTime(now) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                <div class="weather-forecast-date"><h5>Thu</h5></div>
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="50"
+                  class="forecast-icon"
+                />
+                <h6>
+                  <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max"> 18° </span>
+                    <span class="weather-forecast-temperature-min"> 12° </span>
+                  </div>
+                </h6>
+              </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 let time = document.querySelector("#time");
 time.innerHTML = displayTime(new Date());
 
@@ -109,3 +141,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+displayForecast();
